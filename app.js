e8fcf5282ffa285acd59e6586d48e52ff7be2bd8
modori3232 <chsh3232@gmail.com>
2017-11-08 22:23:38 +0900

    home pc

---
+++ app.js
@@ -0,0 +1,93 @@
+var express = require('express');//익스프레스를 가져옴
+var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+var expressSession = require('express-session')
+var multer = require('multer');//업로드를 위한 multer를 가져옴
+var fs = require('fs');
+var http = require('http');
+var path = require('path');
+var app = express();
+var mime = require('mime');
+
+var audioMerge = require('./src/audioMerge.js');
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.set('port', process.env.PORT || 3000);
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+});
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer({ storage: storage }).any());
+
+app.post('/process/photo', function (req, res) {
+
+    var files = req.files;
+    //현재의 파일 정보를 저장할 변수 선언
+    var originalname = '', name = '', mimetype = '', size = 0;
+    var fileNames = [];
+    const path = './uploads/'
+
+    if (Array.isArray(files)) {
+        console.log('배열에 들어 있는 파일 개수: %d', files.length);
+
+        for (var index = 0; index < files.length; index++) {
+            originalname = files[index].originalname;
+            name = files[index].name;
+            mimetype = files[index].mimetype;
+            size = files[index].size;
+            fileNames.push(path + originalname);
+        }
+    } else {
+        console.log('파일개수:1');
+
+        originalname = files[index].originalname;
+        name = files[index].name;
+        mimetype = files[index].mimetype;
+        size = files[index].size;
+        fileNames.push(path + originalname);
+    }
+    console.log('현재의 파일 정보:' + originalname + ',' + name + ',' + mimetype + ',' + size);
+    audioMerge.merge(req, res, fileNames);
+})
+
+app.get('/wait', function (req, res) {
+    res.send('Wait');
+});
+
+app.get('/success', function (req, res) {
+    res.render('success');
+});
+
+app.get('/download/:id', function (req, res) {
+
+    var id = req.params.id;
+    var file = './results/result.mp3';
+    if (id != '1') {
+        file = './results/result.json';
+    }
+
+    filename = path.basename(file), mimetype = mime.getType(file);
+    res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+    res.setHeader('Content-type', mimetype);
+
+    var filestream = fs.createReadStream(file);
+    filestream.pipe(res);
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Server Start Port ' + app.get('port'));
+});
---
+++ src/audioMerge.js
@@ -0,0 +1,51 @@
+
+const fs = require("fs");
+const path = require("path");
+const mime = require("mime");
+
+exports.merge = function (req, res, files) {
+    var audiosprite = require('audiosprite');
+    var opts = { output: './results/result', gap: 0, export: 'mp3' };
+
+    audiosprite(files, opts, function (err, obj) {
+        if (err) return console.error(err);
+
+        console.log(JSON.stringify(obj, null, 2))
+
+        let strJson = JSON.stringify(obj, null, 2)
+        exportJson(req, res, strJson);
+    });
+}
+
+exportJson = function (req, res, strJson) {
+    console.log("Going to write into existing file");
+    fs.writeFile('./results/result.json', strJson, function (err) {
+        if (err) {
+            return console.error(err);
+        }
+
+        console.log("Data written successfully!");
+        console.log("Let's read newly written data");
+
+        fs.readFileSync('./results/result.json', function (err, data) {
+            if (err) {
+                return console.error(err);
+            }
+            console.log("Asynchronous read: " + data.toString());
+        })
+
+        downloadResults(req, res);
+    });
+}
+
+downloadResults = function (req, res) {
+
+    const mp3FilePath = './results/result.mp3';
+    const jsonFilePath = './results/result.json';
+
+    filename = path.basename(mp3FilePath), mimetype = mime.getType(mp3FilePath);
+    res.send( mp3FilePath );
+};
+
+
+

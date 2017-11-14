c3c2826419f014e1e14081c7cb639fec481e2a55
modori3232 <chsh3232@gmail.com>
2017-11-14 12:00:38 +0900

    change format to createjs

--- src/audioMerge.js
+++ src/audioMerge.js
@@ -5,7 +5,7 @@ const mime = require("mime");
 
 exports.merge = function (req, res, files) {
     var audiosprite = require('audiosprite');
-    var opts = { output: './results/result', gap: 0.5, export: 'mp3', channels:2, bitrate:96 };
+    var opts = { output: './results/result', gap: 0.5, export: 'mp3', channels:2, bitrate:96, format:createjs };
 
     audiosprite(files, opts, function (err, obj) {
         if (err) return console.error(err);
